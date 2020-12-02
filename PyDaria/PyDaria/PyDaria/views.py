108e68f4f2b4b7f4741ff8c34372c80e0c137512
Pedro "Spark" Fernandes <42358861+PedroFernandesSpark@users.noreply.github.com>
2020-12-02 13:31:00 -0300

    linkado bd com front de cadastro user e create produto, precisa ser testado

--- PyDaria/PyDaria/PyDaria/views.py
+++ PyDaria/PyDaria/PyDaria/views.py
@@ -7,6 +7,8 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template, request, redirect, url_for
 from PyDaria import app
+import backend.database
+
 
 logado = False
 nome = 'Nome do cliente'
@@ -24,9 +26,14 @@ def home():
     )
 
 @app.route('/signin', methods=['GET', 'POST'])
-def contact():
-    if request.method == 'POST':
-        return request.form
+def singup():
+    if request.method == 'POST' & request.form:
+        cpf = request.form['cpf']
+        name = request.form['name']
+        name = request.form['email']
+        telephone = request.form['phone']
+        password = request.form['password']
+        database.add_client(cpf,name,email.telephone,password)
     else:
         return render_template(
             'signin.html',
@@ -59,13 +66,20 @@ def productadm():
         year=datetime.now().year,
     )
 
-@app.route('/backoffice/produtos/create')
+@app.route('/backoffice/produtos/create', methods=['GET','POST'])
 def productcreate():
-    return render_template(
-        'backoffice_product_create.html',
-        title='Backoffice dos produtos',
-        year=datetime.now().year,
-    )
+    if request.method = 'POST':
+        name = request.form['nome_produto']
+        descricao = request.form['descricao']
+        img = request.form['image']
+        qtd = request.form['stock']
+        database.add_product(name, price, img, qtd)
+    else:
+        return render_template(
+            'backoffice_product_create.html',
+            title='Backoffice dos produtos',
+            year=datetime.now().year,
+        )
 
 @app.route('/produto', methods=['GET','POST'])
 def produto():
